e039930bdb3057a176ff48abf464cd77ff37ba82
John Apale <74929433+johnApale@users.noreply.github.com>
2025-12-09 11:17:37 -0800

    changed controller to lowercase

--- src/AiGroupChat.API/Controllers/GroupsController.cs
+++ src/AiGroupChat.API/Controllers/GroupsController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace AiGroupChat.API.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/groups")]
 [Authorize]
 public class GroupsController : ControllerBase
 {
